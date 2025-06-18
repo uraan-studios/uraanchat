f2510641981867880cf4ada2ddfcad0a1bcfa8ce
Yasin Shah <yas1nshah@yahoo.com>
2025-06-18 23:27:05 +0500

    last min fix

--- app/auth/[pathname]/view.tsx
+++ app/auth/[pathname]/view.tsx
@@ -5,7 +5,7 @@ import { AuthCard } from "@daveyplate/better-auth-ui"
 export function AuthView({ pathname }: { pathname: string }) {
     return (
         <main className="container flex grow flex-col items-center justify-center gap-3 self-center p-4 md:p-6">
-            <AuthCard pathname={pathname} className="max-w-md"/>
+            <AuthCard pathname={pathname} className="max-w-md overflow-clip"/>
         </main>
     )
 }
